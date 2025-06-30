2f8746512002ffb649ef6c22369db0826012572e
uvlad7 <uvlad7@gmail.com>
2025-06-30 15:03:55 +0300

    add spec

--- spec/ffi_llvm_jit_spec.rb
+++ spec/ffi_llvm_jit_spec.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe FfiLlvmJit do
-  it "has a version number" do
+  let(:ffi_llvm_jit_lib) do
+    Module.new.tap do |mod|
+      mod.extend described_class::Library
+      mod.ffi_lib ::FFI::Library::LIBC
+
+      mod.attach_function :strlen, :strlen, [:string], :size_t
+    end
+  end
+
+  it 'has a version number' do
     expect(FfiLlvmJit::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'calculated strlen' do
+    expect(ffi_llvm_jit_lib.strlen('Hello from FFI LLVM JIT!')).to eq 24
   end
 end
